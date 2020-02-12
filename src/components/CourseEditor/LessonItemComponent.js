5d2f7a700c0e666236e5b28c3a57b5697837cc4e
Gcc <gcc@Gccs-MBP.verizon.net>
2020-02-12 01:10:08 -0500

    delete .*itemComponent.js

--- src/components/CourseEditor/LessonItemComponent.js
+++ src/components/CourseEditor/LessonItemComponent.js
@@ -1,15 +1,37 @@
-import React from "react";
-import CourseTableRowComponent from "../CourseTableRowComponent";
-
-
-const LessonItemComponent = ({lesson}) => {
-    return (
-        <li className="nav-item">
-            <a className="nav-link wbdv-page-tab" href="#">
-                <b>{lesson.title}</b>
-            </a>
-        </li>
-    )
-}
-
-export default LessonItemComponent
+// import React from "react";
+// import CourseTableRowComponent from "../CourseTableRowComponent";
+//
+//
+// class LessonTabs extends React.Component {
+//
+//     componentDidMount() {
+//         this.props.findLessonsForModule(this.props.moduleId)
+//     }
+//
+//     componentDidUpdate(prevProps, prevState, snapshot) {
+//         if(this.props.moduleId !== prevProps.moduleId) {
+//             this.props.findLessonsForModule(this.props.moduleId)
+//         }
+//     }
+//
+//     state = {
+//         selectedLessonId: '',
+//         editingLessonId: '',
+//         lesson: {
+//             title: '',
+//             _id: ''
+//         }
+//     }
+//
+//     render() {
+//         return
+//         <li className="nav-item">
+//             <a className="nav-link wbdv-page-tab" href="#">
+//                 <b>{lesson.title}</b>
+//             </a>
+//         </li>
+//
+//     }
+// }
+//
+// export default LessonItemComponent
--- src/components/CourseEditor/ModuleItemComponent.js
+++ src/components/CourseEditor/ModuleItemComponent.js
@@ -1,16 +1,89 @@
-import React from "react";
-import './CourseEditor.css'
-
-const ModuleItemComponent = ({module}) => {
-    return (
-        <div className="wbdv-module-item nav-item">
-            <a className="nav-link  wbdv-module-item-title" href="#">
-                {module.title}
-                <button className="btn" className="wbdv-module-item-delete-btn"><i
-                    className="fas fa-times fa-lg wbdv-module-times"></i></button>
-            </a>
-        </div>
-    )
-}
-
-export default ModuleItemComponent
+// import React from "react";
+// import './CourseEditor.css'
+//
+// class ModuleItemComponent extends React.Component{
+//     state = {
+//         editing: false,
+//         module: this.props.module
+//     }
+//
+//     toggle = () => {
+//         this.setState((prevState) => {
+//             if (prevState.editing === true) {
+//                 return {
+//                     editing: false
+//                 }
+//             } else {
+//                 return {
+//                     editing: true
+//                 }
+//             }
+//         })
+//     }
+//     render(){
+//         return(
+//         <div>
+//             {
+//                 !(this.props.cur === this.state.module._id) &&
+//                 <div>
+//                     <div className="wbdv-module-item container-fluid" onClick={() => {
+//                         this.props.setCurHi(this.state.module._id)
+//                     }}>
+//                         <a className="btn">
+//                             <span className="wbdv-module-item-title">{this.state.module.title}</span>
+//                         </a>
+//                         <a className="wbdv-module-item-delete-btn btn">
+//                             <i className="fas fa-pencil-alt wbdv-edit-module"/>
+//                         </a>
+//                     </div>
+//                 </div>
+//             }
+//             {
+//                 (this.props.cur === this.state.module._id) &&
+//                 <div className="wbdv-module-item-highlighted container-fluid">
+//                     {
+//                         !this.state.editing &&
+//                         <div>
+//                             <a className="wbdv-module-item-title btn">
+//                                 {this.state.module.title}
+//                             </a>
+//                             <a className="wbdv-module-item-delete-btn btn" onClick={() => {this.toggle()}}>
+//                                 <i className="fas fa-pencil-alt wbdv-edit-module"/>
+//                             </a>
+//                         </div>
+//                     }
+//                     {
+//                         this.state.editing &&
+//                         <div>
+//                             <input onChange={(e)=>this.setState({
+//                             module: {
+//                                 ...this.state.module,
+//                                 title: e.target.value
+//                             }
+//                         })}
+//                             value={this.state.module.title}/>
+//                             <div className="delete-and-save-btn">
+//                                 <i className="fas fa-times btn" onClick={() => {
+//                                      this.setState({editing: false})
+//                                      this.props.deleteModule(this.state.module._id)
+//                                 }}>
+//                                 </i>
+//                                 <i className="fas fa-check btn" onClick={
+//                                     ()=>{
+//                                         this.props.updateModule(this.state.module._id,this.state.module)
+//                                             .then(status=>{})
+//                                         this.setState({editing: false})
+//                                     }
+//                                 }
+//                                 ></i>
+//                             </div>
+//                         </div>
+//                     }
+//                 </div>
+//             }
+//         </div>
+//     )}
+//
+// }
+//
+// export default ModuleItemComponent
